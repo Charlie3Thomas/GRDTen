506d4d5ba9a31f490c3fdb53e07bc44b964ea24b
Charlie <charlie3thomas@live.uwe.ac.uk>
2022-11-04 14:14:45 +0000

    [GRDTen] Automatic childing of data container objects to relevant country models in scene

--- GRDTen/Assets/Scripts/CountryMatchUp.cs
+++ GRDTen/Assets/Scripts/CountryMatchUp.cs
@@ -1,23 +1,38 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CountryMatchUp : MonoBehaviour
 {
-    [SerializeField] private GameObject map;
+    [SerializeField] private GameObject globe;
     [SerializeField] private GameObject data;
-    [SerializeField] private List<GameObject> _countries_models = new List<GameObject>();
-    private List<GameObject> _countries_data = new List<GameObject>();
+    //[SerializeField] private List<GameObject> _countries_models = new List<GameObject>();
+    //[SerializeField] private List<GameObject> _countries_data = new List<GameObject>();
+    private Transform[] globe_country_models;
+    private CountryDataLoader[] data_country_prefabs;
 
     private void Start()
     {
-        //foreach (GameObject c_model in map.GetComponentsInChildren<GameObject>())
-        //{
-        //    _countries_models.Add(c_model);
-        //}
-        //foreach (GameObject c_data in data.GetComponentsInChildren<GameObject>())
-        //{
-        //    _countries_models.Add(c_data);
-        //}
+        globe_country_models = globe.GetComponentsInChildren<Transform>();
+        data_country_prefabs = data.GetComponentsInChildren<CountryDataLoader>();
+        for (int i = 0; i < globe_country_models.Length; i++) // Loop through all Globe children
+        {
+            if(globe_country_models[i].name.Length == 3) // Only perform checks on Globe children with 3 letter names (iso3)
+            {
+                //Debug.Log(globe_country_models[i].name);
+                for (int j = 0; j < data_country_prefabs.Length; j++) // For every country data object
+                {
+                    //Debug.Log(data_country_prefabs[j].c_code);
+
+                    if (globe_country_models[i].name == data_country_prefabs[j].c_code)
+                    {
+                        //Debug.Log("Match");
+                        data_country_prefabs[j].transform.parent = globe_country_models[i].transform;
+                    }
+                }
+            }
+        }
     }
 }
