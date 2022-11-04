7d207545a830c104d4c8e74fd211d68399f0843b
Charlie <charlie3thomas@live.uwe.ac.uk>
2022-11-04 19:58:29 +0000

    [GRDTen] Conversion of string data to int, fixes to .csv files with incompatibilities

--- GRDTen/Assets/Scripts/Data Scripts/CropDataContainer.cs
+++ GRDTen/Assets/Scripts/Data Scripts/CropDataContainer.cs
@@ -1,151 +1,137 @@
+using Palmmedia.ReportGenerator.Core.Parser;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using UnityEditor;
 using UnityEngine;
+
 
 public class CropDataContainer : MonoBehaviour
 {
-    [SerializeField] private CropList country_data = new CropList();
-    [SerializeField] private TextAsset[] crop_data_sets; 
-    private SortedDictionary<string, CropList> crop_data_container = 
-        new SortedDictionary<string, CropList>(); // Really don't reccomend serializing this
+    [SerializeField] private TextAsset[] crop_data_sets;
 
-    #region Crop Data Containers
-    // Also thoroughly advise against serializing these:
-        private CropList apple = new CropList();
-        private CropList avocado = new CropList();
-        private CropList banana = new CropList();
-        private CropList barley = new CropList();
-        private CropList bean = new CropList();
-        private CropList cashew = new CropList();
-        private CropList cassava = new CropList();
-        private CropList cereal = new CropList();
-        private CropList cocoa_bean = new CropList();
-        private CropList coffee_bean = new CropList();
-        private CropList grapes = new CropList();
-        private CropList maize = new CropList();
-        private CropList orange = new CropList();
-        private CropList palm_oil = new CropList();
-        private CropList pea = new CropList();
-        private CropList potato = new CropList();
-        private CropList rapeseed = new CropList();
-        private CropList rice = new CropList();
-        private CropList rye = new CropList();
-        private CropList seasame = new CropList();
-        private CropList soybean = new CropList();
-        private CropList sugar_beet = new CropList();
-        private CropList sugar_cane = new CropList();
-        private CropList sunflower_seed = new CropList();
-        private CropList sweet_potato = new CropList();
-        private CropList tea = new CropList();
-        private CropList tobacco = new CropList();
-        private CropList tomato = new CropList();
-        private CropList wheat = new CropList();
-        private CropList yams = new CropList();
-    #endregion Crop Data Containers
+    private Dictionary<string, TextAsset> cds_dictionary = new Dictionary<string, TextAsset>();
 
-    [System.Serializable]
-    public class CropData
+    private Dictionary<string,
+            Dictionary<string,
+            Dictionary<int, long>>> crops;
+
+    private void PopulateCDSDictionary()
     {
-        public string cd_entity;
-        public string cd_year;
-        public string cd_tonnes;
-    }
-
-    [System.Serializable]
-    public class CropList
-    {
-        public CropData[] crops;
+        cds_dictionary.Add("apple", crop_data_sets[0]);
+        cds_dictionary.Add("avocado", crop_data_sets[1]);
+        cds_dictionary.Add("banana", crop_data_sets[2]);
+        cds_dictionary.Add("barley", crop_data_sets[3]);
+        cds_dictionary.Add("bean", crop_data_sets[4]);
+        cds_dictionary.Add("cashew", crop_data_sets[5]);
+        cds_dictionary.Add("cassava", crop_data_sets[6]);
+        cds_dictionary.Add("cereal", crop_data_sets[7]);
+        cds_dictionary.Add("cocoa bean", crop_data_sets[8]);
+        cds_dictionary.Add("coffee bean", crop_data_sets[9]);
+        cds_dictionary.Add("grapes", crop_data_sets[10]);
+        cds_dictionary.Add("maize", crop_data_sets[11]);
+        cds_dictionary.Add("orange", crop_data_sets[12]);
+        cds_dictionary.Add("palm oil", crop_data_sets[13]);
+        cds_dictionary.Add("pea", crop_data_sets[14]);
+        cds_dictionary.Add("potato", crop_data_sets[15]);
+        cds_dictionary.Add("rapeseed", crop_data_sets[16]);
+        cds_dictionary.Add("rice", crop_data_sets[17]);
+        cds_dictionary.Add("rye", crop_data_sets[18]);
+        cds_dictionary.Add("seasame", crop_data_sets[19]);
+        cds_dictionary.Add("soybean", crop_data_sets[20]);
+        cds_dictionary.Add("sugar beet", crop_data_sets[21]);
+        cds_dictionary.Add("sugar cane", crop_data_sets[22]);
+        cds_dictionary.Add("sunflower seed", crop_data_sets[23]);
+        cds_dictionary.Add("sweet potato", crop_data_sets[24]);
+        cds_dictionary.Add("tea", crop_data_sets[25]);
+        cds_dictionary.Add("tobacco", crop_data_sets[26]);
+        cds_dictionary.Add("tomato", crop_data_sets[27]);
+        cds_dictionary.Add("wheat", crop_data_sets[28]);
+        cds_dictionary.Add("yams", crop_data_sets[29]);
     }
 
     private void PopulateCropDataContainer()
     {
-        crop_data_container.Add("apple", apple);
-        crop_data_container.Add("avocado", avocado);
-        crop_data_container.Add("banana", banana);
-        crop_data_container.Add("barley", barley);
-        crop_data_container.Add("bean", bean);
-        crop_data_container.Add("cashew", cashew);
-        crop_data_container.Add("cassava", cassava);
-        crop_data_container.Add("cereal", cereal);
-        crop_data_container.Add("cocoa bean", cocoa_bean);
-        crop_data_container.Add("coffee bean", coffee_bean);
-        crop_data_container.Add("grapes", grapes);
-        crop_data_container.Add("maize", maize);
-        crop_data_container.Add("orange", orange);
-        crop_data_container.Add("palm oil", palm_oil);
-        crop_data_container.Add("pea", pea);
-        crop_data_container.Add("potato", potato);
-        crop_data_container.Add("rapeseed", rapeseed);
-        crop_data_container.Add("rice", rice);
-        crop_data_container.Add("rye", rye);
-        crop_data_container.Add("seasame", seasame);
-        crop_data_container.Add("soybean", soybean);
-        crop_data_container.Add("sugar beet", sugar_beet);
-        crop_data_container.Add("sugar cane", sugar_cane);
-        crop_data_container.Add("sunflower seed", sunflower_seed);
-        crop_data_container.Add("sweet potato", sweet_potato);
-        crop_data_container.Add("tea", tea);
-        crop_data_container.Add("tobacco", tobacco);
-        crop_data_container.Add("tomato", tomato);
-        crop_data_container.Add("wheat", wheat);
-        crop_data_container.Add("yams", yams);
+        crops.Add("apple",          new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("avocado",        new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("banana",         new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("barley",         new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("bean",           new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("cashew",         new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("cassava",        new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("cereal",         new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("cocoa bean",     new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("coffee bean",    new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("grapes",         new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("maize",          new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("orange",         new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("palm oil",       new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("pea",            new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("potato",         new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("rapeseed",       new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("rice",           new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("rye",            new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("seasame",        new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("soybean",        new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("sugar beet",     new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("sugar cane",     new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("sunflower seed", new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("sweet potato",   new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("tea",            new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("tobacco",        new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("tomato",         new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("wheat",          new Dictionary<string, Dictionary<int, long>>());
+        crops.Add("yams",           new Dictionary<string, Dictionary<int, long>>());
+        
 
     }
-    private void ReadCSVCrop(TextAsset _txt_asset, CropList _crop_list)
+    private void ReadCSVCrop(string _key)
     {
-        string[] data = _txt_asset.text.Split(new string[] { ",", "\n" }, StringSplitOptions.None);
-        int table_size = data.Length / 4;
-        _crop_list.crops = new CropData[table_size];
 
-        for (int i = 0; i < table_size; i++)
+        string[] data = cds_dictionary[_key].text.Split(new string[] { ",", "\n" }, StringSplitOptions.None);
+        int table_size = data.Length / 4;
+        Debug.Log(_key + ": " + table_size);
+
+        for (int i = 1; i < table_size - 1; i++)
         {
-            _crop_list.crops[i] = new CropData();
-            _crop_list.crops[i].cd_entity = data[(4 * i)];
-            //crop_list.crop[i].cd_code = data[(4 * i) + 1];
-            _crop_list.crops[i].cd_year = data[(4 * i) + 2];
-            _crop_list.crops[i].cd_tonnes = data[(4 * i) + 3];
+            string cd_name = data[(4 * i)];
+            //Debug.Log(data[(4 * i) + 2]);
+            Debug.Log("              " + _key + cd_name + " " + i);
+            int cd_year = Int32.Parse(data[(4 * i) + 2]);
+            long cd_tonnes = Int64.Parse(data[(4 * i) + 3]);
+
+            if (!crops[_key].ContainsKey(cd_name))
+            {
+                crops[_key][cd_name] = new Dictionary<int, long>();
+            }
+
+            crops[_key][cd_name][cd_year] = cd_tonnes;
         }
     }
 
     private void Start()
     {
+        crops = new Dictionary<string,
+                    Dictionary<string,
+                    Dictionary<int, long>>>();
+
+
+        PopulateCDSDictionary();
         PopulateCropDataContainer();
 
-        int i = 0;
-        foreach(var (key, value) in crop_data_container)
+        foreach(var key in crops.Keys)
         {
-            ReadCSVCrop(crop_data_sets[i++], value);            
+            ReadCSVCrop(key);            
         }
 
-        GetDataForCountry("apple", "Afghanistan", "2000");
+        GetDataForCountry("apple", "Afghanistan", 2000);
     }
 
-    public void GetDataForCountry(string _crop, string _coutnry_name, string _production_year)
+    public void GetDataForCountry(string _crop, string _coutnry_name, int _production_year)
     {
-        foreach (CropData data in crop_data_container[_crop].crops)
-        {
-            if (data.cd_entity == _coutnry_name && data.cd_year == _production_year)
-            {
-                Debug.Log(data.cd_tonnes);
-            }
-        }
-
-        //for (int i = 0; i < crop_data_container.Count; i++) // For each crop type
-        //{
-        //   for (int j = 0; j < (crop_data_container[i].crops.Length); j++) // For each crop entry
-        //    {
-        //        if (crop_data_container[i].crops[j].cd_entity == country_name) // If input name equals data entry entity(name)
-        //        {
-        //            if (crop_data_container[i].crops[j].cd_year == production_year)
-        //            {
-        //                
-        //            }
-        //        }
-        //    }
-        //}
-
+        long data = crops[_crop][_coutnry_name][_production_year];
+        Debug.Log(data);
     }
 }
