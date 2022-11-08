d689c49116066732f84c21e669360bada020bee4
Ramy-Abousaif <ramydragolord@gmail.com>
2022-11-08 09:02:01 +0000

    Blackhole animations and collectable manager

--- GRDTen/Assets/Scripts/PhysicsLauncher.cs
+++ GRDTen/Assets/Scripts/PhysicsLauncher.cs
@@ -25,7 +25,7 @@ public class PhysicsLauncher : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        physicsObjectManager = GameObject.Find("PhysicsObjectManager");
+        physicsObjectManager = GameObject.FindGameObjectWithTag("PhysicsObjectManager");
         StartCoroutine(ShootObjects());
     }
 
